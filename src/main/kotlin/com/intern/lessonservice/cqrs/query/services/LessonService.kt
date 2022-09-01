3b31721e44ee2efc3e43305272d33aa738d27289
thuyhang <thuyhangfr01@gmail.com>
2022-09-01 21:53:13 +0700

    get lÃesson by id

--- src/main/kotlin/com/intern/lessonservice/cqrs/query/services/LessonService.kt
+++ src/main/kotlin/com/intern/lessonservice/cqrs/query/services/LessonService.kt
@@ -2,6 +2,7 @@ package com.intern.lessonservice.cqrs.query.services
 
 import com.intern.lessonservice.cqrs.query.domain.Lesson
 import com.intern.lessonservice.cqrs.query.repositories.LessonRepository
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 
 @Component
@@ -9,5 +10,11 @@ class LessonService (val lessonRepository: LessonRepository)
 {
     fun findAll(): MutableList<Lesson> {
         return lessonRepository.findAll()
+    }
+
+    fun findById(lessonId: Long): ResponseEntity<Lesson> {
+        return lessonRepository.findById(lessonId).map { lesson ->
+            ResponseEntity.ok(lesson)
+        }.orElse(ResponseEntity.notFound().build())
     }
 }
